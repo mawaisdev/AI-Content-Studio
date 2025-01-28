a9424248935946dd007e42261da163af3a5e7a73
Muhammad Awais <m.awais2030@gmail.com>
2025-01-29 00:31:20 +0500

    Removed Comments

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -7,32 +7,6 @@ import {
   MemoryHealthIndicator,
 } from '@nestjs/terminus';
 import { PrismaService } from '../prisma/prisma.service';
-// import { InjectRedis } from '@nestjs-modules/ioredis';
-// import Redis from 'ioredis';
-
-// @Public()
-// @Controller('health')
-// export class HealthController {
-//   constructor(
-//     private health: HealthCheckService,
-//     private memory: MemoryHealthIndicator,
-//     private prisma: PrismaService,
-//     @InjectRedis() private readonly redis: Redis,
-//   ) {}
-
-//   @Get()
-//   @HealthCheck()
-//   check() {
-//     return this.health.check([
-//       async () => this.memory.checkHeap('memory_heap', 200 * 1024 * 1024),
-//       async () =>
-//         this.prisma.$queryRaw`SELECT 1`.then(() => ({
-//           prisma: { status: 'up' },
-//         })),
-//       async () => this.redis.ping().then(() => ({ redis: { status: 'up' } })),
-//     ]);
-//   }
-// }
 
 import { Inject } from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
